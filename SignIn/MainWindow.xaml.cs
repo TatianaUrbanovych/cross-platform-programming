141e8ad2a18119265f1a1827882ab3354a4a6284
Tatiana <tanya.u.2003@gmail.com>
2022-10-05 16:44:08 +0100

    First commit

---
+++ SignIn/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace SignIn
+{
+    static class DB
+    {
+        //public static string TestDB = "Admin";
+        public static Dictionary<string, string> TestDB = new Dictionary<string, string>()
+            {
+                { "admin@admin", "admin"},
+                { "user1@ukr.net", "user1"},
+                { "user2@gmail.com", "user2"}
+            };
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(Email.Text) && !string.IsNullOrWhiteSpace(Password.Password))
+            { 
+                    if (DB.TestDB.ContainsKey(Email.Text) && DB.TestDB.ContainsValue(Password.Password))
+                    {
+                        MessageBox.Show("Welcome!");
+                    }
+                    else {
+                        MessageBox.Show("Invalid credentials!");
+                    }
+                Email.Clear();
+                Password.Clear();
+            }
+            else {
+                MessageBox.Show("Заповніть обидва поля");
+            }
+        }
+    }
+}
